c05ca3db17a10859da518132154795e5d8a075b3
Shafiq Sharifi <213167585+ShafiqSharifi@users.noreply.github.com>
2026-08-17 21:10:53 -0700

    Complete assignment 07

--- js/index.js
+++ js/index.js
@@ -13,5 +13,14 @@ copyright.innerHTML =
   "&copy; " + thisYear + " Mohammad Shafiq Sharifi. All rights reser ";
 footer.appendChild(copyright);
 
+const skills = ["JavaScript", "HTML", "CSS", "GitHub", "Adobe Photoshop"];
 
+let skillsSection = document.getElementById("skills");
+let skillsList = skillsSection.querySelector("ul");
 
+for (let i = 0; i < skills.length; i++) {
+  let skill = document.createElement("li");
+  skill.innerText = skills[i];
+  skill.classList.add("skills-item");
+  skillsList.appendChild(skill);
+}
